44944b480bbecdd5b31fcb3dde4a06730f26c0e2
Sergei Kozelko <kozelkos@yandex.ru>
2018-06-10 13:19:32 +0300

    Added iterators (all)

--- list.h
+++ list.h
@@ -11,12 +11,75 @@
 template <typename T>
 class list {
     struct node {
-        std::unique_ptr<T> ptr; // node owns object
+        std::unique_ptr<T> data; // node owns object
         std::unique_ptr<node> next; // node owns next node
         node* prev; // always valid because we are owned by someone
 
-        node() : ptr(nullptr), next(nullptr), prev(nullptr) {};
-        explicit node(const T& data) : ptr(std::make_unique<T>(data)), next(nullptr), prev(nullptr) {};
+        node() : data(nullptr), next(nullptr), prev(nullptr) {};
+        explicit node(const T& data) : data(std::make_unique<T>(data)), next(nullptr), prev(nullptr) {};
+    };
+
+    template <bool b, typename T1, typename T2>
+    struct template_if {};
+
+    template <typename T1, typename T2>
+    struct template_if<true, T1, T2> { using type = T1; };
+
+    template <typename T1, typename T2>
+    struct template_if<false, T1, T2> { using type = T2; };
+
+
+    template <bool is_const>
+    struct iterator_impl {
+        using iterator_category = std::bidirectional_iterator_tag;
+        using difference_type = size_t;
+        using pointer = typename template_if<is_const, const T*, T*>::type;
+        using reference = typename template_if<is_const, const T&, T&>::type;
+        using value_type = T;
+
+        iterator_impl() = default;
+        explicit iterator_impl(node* ptr) : ptr(ptr) {}
+
+        iterator_impl operator++(int) {
+            iterator_impl tmp = *this;
+            ptr = (ptr->next).get();
+            return tmp;
+        }
+
+        iterator_impl& operator++() {
+            ptr = (ptr->next).get();
+            return *this;
+        }
+
+        iterator_impl operator--(int) {
+            iterator_impl tmp = *this;
+            ptr = ptr->prev;
+            return tmp;
+        }
+
+        iterator_impl operator--() {
+            ptr = ptr->prev;
+            return *this;
+        }
+
+        reference operator*() const {
+            return *(ptr->data);
+        }
+
+        pointer operator->() const {
+            return (ptr->data).get();
+        }
+
+        friend bool operator==(const iterator_impl& a, const iterator_impl& b) {
+            return a.ptr == b.ptr;
+        }
+
+        friend bool operator!=(const iterator_impl& a, const iterator_impl& b) {
+            return !(a == b);
+        }
+
+    private:
+        node* ptr;
     };
 
     std::unique_ptr<node> head;
@@ -24,7 +87,16 @@ class list {
     size_t sz;
 
 public:
+    using iterator = iterator_impl<false>;
+    using const_iterator = iterator_impl<true>;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
     list() : head(nullptr), tail(nullptr), sz(0) {}
+
+    list(const list<T>& other) :list() {
+        for (const auto& val : other) { this->push_back(val); }
+    }
 
     void push_back(const T& data) {
         if (tail == nullptr) {
@@ -62,14 +134,14 @@ public:
         assert(sz != 0);
         // same as assert(tail != nullptr && tail->prev != nullptr);
 
-        return *(tail->prev->ptr);
+        return *(tail->prev->data);
     }
 
     const T& back() const {
         assert(sz != 0);
         // same as assert(tail != nullptr && tail->prev != nullptr);
 
-        return *(tail->prev->ptr);
+        return *(tail->prev->data);
     }
 
     void push_front(const T& data) {
@@ -106,13 +178,13 @@ public:
     T& front() {
         assert(sz != 0);
 
-        return *(head->ptr);
+        return *(head->data);
     }
 
     const T& front() const {
         assert(sz != 0);
 
-        return *(head->ptr);
+        return *(head->data);
     }
 
     bool empty() const {
@@ -121,6 +193,38 @@ public:
 
     size_t size() const {
         return sz;
+    }
+
+    iterator begin() const {
+        return iterator(head.get());
+    }
+
+    iterator end() const {
+        return iterator(tail);
+    }
+
+    const_iterator cbegin() const {
+        return const_iterator(head.get());
+    }
+
+    const_iterator cend() const {
+        return const_iterator(tail);
+    }
+
+    reverse_iterator rbegin() const {
+        return std::make_reverse_iterator(end());
+    }
+
+    reverse_iterator rend() const {
+        return std::make_reverse_iterator(begin());
+    }
+
+    const_reverse_iterator crbegin() const {
+        return std::make_reverse_iterator(cend());
+    }
+
+    const_reverse_iterator crend() const {
+        return std::make_reverse_iterator(cbegin());
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -25,9 +25,33 @@ int main() {
     string_list.push_front("hello");
     string_list.push_back("of strings");
 
-    while (!string_list.empty()) {
-        std::cout << string_list.front();
-        string_list.pop_front();
+    std::cout << (string_list.begin()->back());
+
+    for (const auto& word : string_list) {
+        std::cout << word;
+    }
+    std::cout << std::endl;
+
+    for (auto rit = string_list.rbegin(); rit != string_list.rend(); ++rit) {
+        std::cout << *rit << " ";
+        std::cout.flush();
+    }
+    std::cout << std::endl;
+
+    list<list<std::string>> mlist;
+    for (int i = 0; i < 10; i++) {
+        list<std::string> tmp;
+        for (int j = 0; j < 10; j++) {
+            tmp.push_back(std::to_string(i * 10 + j));
+        }
+        mlist.push_back(tmp);
+    }
+
+    for (const auto& tmp : mlist) {
+        for (const auto& s : tmp) {
+            std::cout << s << " ";
+        }
+        std::cout << std::endl;
     }
 
     return 0;
