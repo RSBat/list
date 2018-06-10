b752a9b6bdcbf1701af761b73ba424697bfbcbe1
Sergei Kozelko <kozelkos@yandex.ru>
2018-06-10 11:08:03 +0300

    Init

---
+++ list.h
@@ -0,0 +1,18 @@
+//
+// Created by rsbat on 6/10/18.
+//
+
+#ifndef LIST_LIST_H
+#define LIST_LIST_H
+
+template <typename T>
+class list {
+    struct node {
+
+    };
+
+
+};
+
+
+#endif //LIST_LIST_H
